1488ef19df0bc0a91f69c7b59c11f327e86e9ea1
Julian <juliant908@gmail.com>
2025-06-14 01:09:04 -0500

    updated cloud service styles

--- src/app/features/dashboard/components/cloud-services/cloud-services.component.ts
+++ src/app/features/dashboard/components/cloud-services/cloud-services.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-cloud-services',
   imports: [],
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './cloud-services.component.css'
 })
 export class CloudServicesComponent {
+  // Child views
 
+
+
+  services = ['aws-kinesis', 'aws-lambda', 'aws-dynamo'];
+
+  destroy$ = inject(DestroyRef);
+
+  ngAfterViewInit() {
+    this.animateAwsPipeline();
+  }
+
+  animateAwsPipeline() {
+    let i = 0;
+    interval(250).pipe(takeUntilDestroyed(this.destroy$)).subscribe(
+      () => {
+        if (i > 0) document.getElementById(this.services[i-1])?.classList.remove('active');
+        if (i < this.services.length) {
+            document.getElementById(this.services[i])?.classList.add('active');
+        } else {
+            document.getElementById(this.services[i-1])?.classList.remove('active');
+            i = -1
+        }
+        i++;
+      }
+    )
+  }
 }
